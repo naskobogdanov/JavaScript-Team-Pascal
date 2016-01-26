6b26d387bcfd6e234799ba0cdd44c45d4cb98d82
Fujitzo <xpucto87@gmail.com>
2016-01-26 23:35:18 +0200

    Added questions for 2nd(middle) level.

--- js/quiz2.js
+++ js/quiz2.js
@@ -1,73 +1,73 @@
-var quiz = [{
-  "question": "What is the full form of SoftUni?",
-  "choices": ["Soft Uniform", "Software Unity", "Software University"],
-  "correct": "Software University"
-}, {
-  "question": "Who is the founder of Microsoft?",
-  "choices": ["Bill Gates", "Steve Jobs", "Steve Wozniak"],
-  "correct": "Bill Gates"
-}, {
-  "question": "1 byte = ?",
-  "choices": ["8 bits", "64 bits", "1024 bits"],
-  "correct": "8 bits"
-}, {
-  "question": "The C programming language was developed by?",
-  "choices": ["Brendan Eich", "Dennis Ritchie", "Guido van Rossum"],
-  "correct": "Dennis Ritchie"
-}, {
-  "question": "What does CC mean in emails?",
-  "choices": ["Carbon Copy", "Creative Commons", "other"],
-  "correct": "Carbon Copy"
-}];
-
-//var quiz2 = [{
-//  "question": "What’s the default cluster size in the NTFS file system under Windows?",
-//  "choices": ["2 KB", "4 KB", "16 KB", "256 KB"],
-//  "correct": "4 KB"
+//var quiz = [{
+//  "question": "What is the full form of SoftUni?",
+//  "choices": ["Soft Uniform", "Software Unity", "Software University"],
+//  "correct": "Software University"
 //}, {
-//  "question": "What’s the key combination to bring up the task manager directly in Win 7/ Vista?",
-//  "choices": ["Ctrl+alt+del", "Ctrl+shift+esc", "Ctrl+f1", "Alt+Shift"],
-//  "correct": "Ctrl+shift+esc"
+//  "question": "Who is the founder of Microsoft?",
+//  "choices": ["Bill Gates", "Steve Jobs", "Steve Wozniak"],
+//  "correct": "Bill Gates"
 //}, {
-//  "question": "What is the slowest type of memory?",
-//  "choices": ["RAM", "SWAP FILE", "ROM", "SSD"],
-//  "correct": "SWAP FILE"
+//  "question": "1 byte = ?",
+//  "choices": ["8 bits", "64 bits", "1024 bits"],
+//  "correct": "8 bits"
 //}, {
-//  "question": "Which commonly used in programming symbol is usually missing on most keyboards?",
-//  "choices": ["Pipe", "Asterix", "Tilde", "Percentage"],
-//  "correct": "Pipe"
+//  "question": "The C programming language was developed by?",
+//  "choices": ["Brendan Eich", "Dennis Ritchie", "Guido van Rossum"],
+//  "correct": "Dennis Ritchie"
 //}, {
-//  "question": "For a long time advancement in microprocessor technologies is marked not by increasing frequency" +
-//  " but by decreasing manufacturing process, which leads to improved power efficiency and architecture." +
-//  " What is the latest by Intel (mass production)?",
-//  "choices": ["32nm", "22nm", "14nm", "10nm"],
-//  "correct": "14nm"
-//}, {
-//  "question": "We already have „Acne for dummies” and „Raising cattle for dummies”, but which is the first book of the series?",
-//  "choices": ["Windows for Dummies", "Macintosh for Dummies", "Dos for Dummies", "C++ for Dummies"],
-//  "correct": "Dos for Dummies"
-//
-//}, {
-//  "question": "Which of the below is one of the most recent programming languages?",
-//  "choices": ["Swift", "Groovy", "Windows Power Shell", "Delphi"],
-//  "correct": "Swift"
-//
-//}, {
-//  "question": "In Unix-based OS it is called terminal, how is it called in Windows?",
-//  "choices": ["Port", "Debugger", "Control Panel", "CMD"],
-//  "correct": "CMD"
-//
-//}, {
-//  "question": "In RGB system red is 255, 0, 0. In CMYK it is 0, 100, 100, 0, how about HTML notation?",
-//  "choices": ["0.0.0", "ff0000", "ffffff", "009900"],
-//  "correct": "ff0000"
-//
-//}, {
-//  "question": "What did retina display initially mean when the term was first marketed by Apple?",
-//  "choices": ["Higher pixel count", "Higher pixel density", "Higher contrast and brightness", "Better viewing angles"],
-//  "correct": "Higher pixel count"
-//
+//  "question": "What does CC mean in emails?",
+//  "choices": ["Carbon Copy", "Creative Commons", "other"],
+//  "correct": "Carbon Copy"
 //}];
+
+var quiz = [{
+  "question": "What’s the default cluster size in the NTFS file system under Windows?",
+  "choices": ["2 KB", "4 KB", "16 KB", "256 KB"],
+  "correct": "4 KB"
+}, {
+  "question": "What’s the key combination to bring up the task manager directly in Win 7/ Vista?",
+  "choices": ["Ctrl+alt+del", "Ctrl+shift+esc", "Ctrl+f1", "Alt+Shift"],
+  "correct": "Ctrl+shift+esc"
+}, {
+  "question": "What is the slowest type of memory?",
+  "choices": ["RAM", "SWAP FILE", "ROM", "SSD"],
+  "correct": "SWAP FILE"
+}, {
+  "question": "Which commonly used in programming symbol is usually missing on most keyboards?",
+  "choices": ["Pipe", "Asterix", "Tilde", "Percentage"],
+  "correct": "Pipe"
+}, {
+  "question": "For a long time advancement in microprocessor technologies is marked not by increasing frequency" +
+  " but by decreasing manufacturing process, which leads to improved power efficiency and architecture." +
+  " What is the latest by Intel (mass production)?",
+  "choices": ["32nm", "22nm", "14nm", "10nm"],
+  "correct": "14nm"
+}, {
+  "question": "We already have „Acne for dummies” and „Raising cattle for dummies”, but which is the first book of the series?",
+  "choices": ["Windows for Dummies", "Macintosh for Dummies", "Dos for Dummies", "C++ for Dummies"],
+  "correct": "Dos for Dummies"
+
+}, {
+  "question": "Which of the below is one of the most recent programming languages?",
+  "choices": ["Swift", "Groovy", "Windows Power Shell", "Delphi"],
+  "correct": "Swift"
+
+}, {
+  "question": "In Unix-based OS it is called terminal, how is it called in Windows?",
+  "choices": ["Port", "Debugger", "Control Panel", "CMD"],
+  "correct": "CMD"
+
+}, {
+  "question": "In RGB system red is 255, 0, 0. In CMYK it is 0, 100, 100, 0, how about HTML notation?",
+  "choices": ["0.0.0", "ff0000", "ffffff", "009900"],
+  "correct": "ff0000"
+
+}, {
+  "question": "What did retina display initially mean when the term was first marketed by Apple?",
+  "choices": ["Higher pixel count", "Higher pixel density", "Higher contrast and brightness", "Better viewing angles"],
+  "correct": "Higher pixel count"
+
+}];
 
 
 // define elements
