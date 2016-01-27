84518b886e14144cd1d27e9b4414b587535f24c6
borislav-lychezarov <borislavlychezarov@gmail.com>
2016-01-27 22:10:27 +0200

    some changes

--- FrondPageStyle/styleLevelPages.js
+++ FrondPageStyle/styleLevelPages.js
@@ -1,1 +1,23 @@
 "use strict"
+
+
+$(document).ready(function(){
+
+    $('body').css({
+        'background-image':'url(images/images.jpg)',
+        'font-family':'Arial, "Helvetica", sans-serif',
+        'font-size':'2.0em'
+    });
+
+    $('#wrapper')
+        .css({
+            'border-radius':'50px',
+            'margin':'0 auto',
+            'width':'850px',
+            'height':'600px',
+            'background-color':'rgba(255, 255, 153, 0.9)',
+            'text-align':'center',
+            'position':'relative',
+            'color':'#003366'
+        });
+});
